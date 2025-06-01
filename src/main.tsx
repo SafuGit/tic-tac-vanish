bf90cacc1c8ae15288723ef23cd4a4703e55ce24
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-06-01 20:21:12 +0600

    add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import AuthProvider from './contexts/AuthProvider.tsx'
+import { RouterProvider } from 'react-router'
+import { routes } from './router/routes.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <RouterProvider router={routes}></RouterProvider>
     </AuthProvider>
   </StrictMode>,
 )
---
+++ src/router/routes.tsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../layouts/Root";
+
+export const routes = createBrowserRouter([
+  {path: '/', Component: Root, children: [
+    
+  ]}
+])
